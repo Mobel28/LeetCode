23f86a430a6232c700b23b89aa5045e674b548d5
Mobel <mobelm522@gmail.com>
2025-05-01 00:08:48 +0530

    Time: 4 ms (44.34%), Space: 42.4 MB (71.52%) - LeetHub

---
+++ 0968-beautiful-array/0968-beautiful-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] beautifulArray(int n) {
+        List<Integer> res=new ArrayList<>();
+        res.add(1);
+        while(res.size()<n){
+            List<Integer> temp=new ArrayList<>();
+            for(int x:res){
+                if(2*x-1<=n)temp.add(2*x-1);
+            }
+            for(int x:res){
+                if(2*x<=n)temp.add(2*x);
+            }
+            res=temp;
+        }
+        int []result=new int[n];
+        for(int i=0;i<n;i++)result[i]=res.get(i);
+        return result;
+    }
+}
