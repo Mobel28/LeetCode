587792396e7c45244e9881d2931ef36396a4ee8c
Mobel <mobelm522@gmail.com>
2025-08-25 09:32:02 +0530

    Time: 0 ms (100%), Space: 17.6 MB (91.66%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,17 +1,4 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        res=""
-        rem=0
-        i,j=len(a)-1,len(b)-1
-        while i>=0 or j>=0 or rem>0:
-            r=rem
-            if i>=0:
-                r+=int(a[i])
-                i-=1
-            if j>=0:
-                r+=int(b[j])
-                j-=1
-            res+=str(r%2)
-            rem=r//2
-        return res[::-1]
+       return bin(int(a,2)+int(b,2))[2:]
 
