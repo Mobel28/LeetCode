4ee7b760bbb8ab5cb682b6e664f98dfa43973856
Mobel <mobelm522@gmail.com>
2025-09-20 15:14:27 +0530

    Time: 0 ms (100%), Space: 18 MB (10.01%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.py
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.py
@@ -4,7 +4,7 @@ class Solution:
         for i in s:
             if i=='(':
                 stack.append(i)
-                print(stack)
+                # print(stack)
             else:
                 if stack and stack[-1]=='(':
                     stack.pop()
