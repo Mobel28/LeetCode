27a39b305c35883f7f1a82aff987f8beec9adf3a
Mobel <mobelm522@gmail.com>
2026-07-07 09:59:07 +0530

    Time: 19 ms (16.18%), Space: 20.3 MB (17.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s)==Counter(t)
+        return sorted(s)==sorted(t)
         #TC=O(n)
-        #SC=O(k)
+        #SC=O(n)
