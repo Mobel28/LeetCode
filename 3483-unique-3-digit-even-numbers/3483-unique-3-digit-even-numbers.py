a048d60b414aaae65cdb1044185904e2d2e38f73
Mobel <mobelm522@gmail.com>
2026-09-14 16:02:20 +0530

    Time: 55 ms (30.56%), Space: 19.3 MB (36.2%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
@@ -2,15 +2,25 @@ class Solution:
     def totalNumbers(self, digits: List[int]) -> int:
         freq=Counter(digits)
         count=0
-        for i in  range(100,1000):
-            if i%2!=0:
-                continue
-            tempfreq=Counter(str(i))
-            for j in tempfreq.keys():
-                    if freq[int(j)]<tempfreq[j]:
-                        break
-            else:
-                count+=1
+        for h in range(1,10):
+            for t in range(10):
+                for u in range(0,10,2):
+                    if h==t==u:
+                        if freq[h]>=3:
+                            count+=1
+                    elif h==t:
+                        if freq[h]>=2 and freq[u]>=1:
+                            count+=1
+                    elif h==u:
+                        if freq[h]>=2 and freq[t]>=1:
+                            count+=1
+                    elif t==u:
+                        if freq[t]>=2 and freq[h]>=1:
+                            count+=1
+                    else:
+                        if freq[h]>=1 and freq[t]>=1 and freq[u]>=1:
+                            count+=1
+            
         return count
 
 
